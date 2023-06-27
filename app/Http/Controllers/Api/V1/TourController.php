94268631ef53c01dba461054cd5ad7ca0430d97f
olacodex <aluko798@gmail.com>
2023-06-27 16:41:11 +0100

    admin create travel endpoint ready with tests

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -11,18 +11,6 @@ class TourController extends Controller
 {
     public function index(Travel $travel, ToursListRequest $request)
     {
-        // $request->validate([
-        //     'priceFrom' => 'numeric',
-        //     'priceTo' => 'numeric',
-        //     'dateTo' => 'date',
-        //     'dateFrom' => 'date',
-        //     'sortBy' => Rule::in(['price']),
-        //     'sortOrder' => Rule::in(['asc','desc']),
-        // ], [
-        //     'sortBy' => "The 'sortBy' parameter accepts only 'price' value",
-        //     'sortOrder' => "The 'sortOrder' parameter accepts only 'asc' or 'desc' values",
-        // ]);
-         dd($validatedInput = $request->safe());
         $tours = $travel->tours()
             ->when($request->priceFrom, function ($query) use ($request) {
                 $query->where('price', '>=', $request->priceFrom * 100);
