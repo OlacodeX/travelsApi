6fe4ef5fe531f13422c95d722a49f65f6f753123
Olawale Williams Aluko <38240402+OlacodeX@users.noreply.github.com>
2023-07-01 07:49:47 +0100

    Admin and editor update tour (#6)
    * admin create tours ready with tests
    
    * admin and editor update travel endpoint with test

--- tests/Feature/AdminTravelTest.php
+++ tests/Feature/AdminTravelTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\Role;
 use App\Models\User;
+use App\Models\Travel;
 use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -48,5 +49,32 @@ class AdminTravelTest extends TestCase
             'number_of_days' => 5,
         ]);
         $response->assertStatus(201);
+    }
+
+    public function test_updates_travel_successfully_with_valid_data(): void
+    {
+        
+        $this->seed(RoleSeeder::class);
+        $user = User::factory()->create();
+        $user->roles()->attach(Role::where('name', 'admin')->value('id'));
+        $travel = Travel::factory()->create();
+        $response = $this->actingAs($user)->putJson('/api/v1/admin/travels/'.$travel->id, [
+            'name' => 'Travel name',
+        ]);
+
+        $response->assertStatus(422);
+
+        $response = $this->actingAs($user)->putJson('/api/v1/admin/travels/'.$travel->id, [
+            'name' => 'Travel name updated',
+            'is_public' => 1,
+            'description' => 'something',
+            'number_of_days' => 5,
+        ]);
+        $response->assertStatus(200);
+        $response = $this->get('/api/v1/travels');
+        $response->assertJsonFragment([
+            'name' => 'Travel name updated'
+        ]);
+
     }
 }
