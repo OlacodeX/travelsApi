a7cb27c5c23a572546b297feaa4dfa189f590aa2
olacodex <aluko798@gmail.com>
2023-08-15 10:56:47 +0100

    added dummy unit test

--- tests/Unit/DummyTest.php
+++ tests/Unit/DummyTest.php
@@ -6,5 +6,13 @@ use Tests\TestCase;
 
 class DummyTest extends TestCase
 {
-   
+    /**
+    * A basic test example.
+    *
+    * @return void
+    */
+   public function test_that_true_is_true()
+   {
+       $this->assertTrue(true);
+   }
 }
